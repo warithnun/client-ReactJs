25e4107f19baec26373944280e269b94628ee481
wrt <warithnun.ph@rmuti.ac.th>
2024-08-19 16:10:00 +0700

    pre function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 
 function App() {
   //javascript
-  
+  const wrt = 'wrtzen'
+  function wrt1(){
+    // code
+    return
+  }
+  const wrt2=()=>{
+    return
+  }
+
   return (
     <div className="App">
+      {/* {HTML} */}
       <h1>From CRUD</h1>
+      {wrt}
     </div>
   );
 }
